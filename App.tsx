bb228323497d972c6ac21e41a8a54f1fa56cf3ed
Abdelhalim Ahmed <abdelhalim.ahmed@we-conect.com>
2021-12-19 19:20:51 +0100

    chore: mark done tasks

--- App.tsx
+++ App.tsx
@@ -8,15 +8,16 @@ import Navigation from './navigation';
 /* TODO-LIST:
    1. [x] initialize app (expo), describe features workflow (.30)
    2. [x] add navigation (.30)
-   3. [ ] search for free RSS feeds api
-   4. [ ] RSS feeds list (UI + integration)
-   5. [ ] Entries Feed list (UI + integration)
-   6. [ ] Entry Details Screen
-   7. [ ] Add the name of Feed into header for the list of entries
-   8. [ ] Add the number of items in the entries feed list
-   9. [ ] add error handling for each data fetch from RSS feed
-  10. [ ] add favorites of feeds into local storage
-  11. [ ] add unit testing (jest + react-native-testing-library)
+   3. [x] search for free RSS feeds api (.15) Done with ==> https://www.dw.com/en/rss/s-31500
+   4. [ ] setup context API for manage state and fetch side effect
+   5. [ ] RSS feeds list (UI + integration)
+   6. [ ] Entries Feed list (UI + integration)
+   7. [ ] Entry Details Screen
+   8. [ ] Add the name of Feed into header for the list of entries
+   9. [ ] Add the number of items in the entries feed list
+  10. [ ] add error handling for each data fetch from RSS feed
+  11. [ ] add favorites of feeds into local storage
+  12. [ ] add unit testing (jest + react-native-testing-library)
 */
 
 export default function App() {
